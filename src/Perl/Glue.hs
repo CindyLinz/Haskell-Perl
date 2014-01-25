589aa41a90c94c8966a9a9175d52a292bf2a5d7c
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2014-01-26 03:28:12 +0800

    import ref count functions & scope clean

--- src/Perl/Glue.hs
+++ src/Perl/Glue.hs
@@ -4,10 +4,42 @@ module Perl.Glue
 
 import Foreign
 
+type StrLen = Int
+
 data PerlInterpreter
+type PtrPerl = Ptr PerlInterpreter
+
+data SV
+type PtrSV = Ptr SV
+
+data AV
+type PtrAV = Ptr AV
+
+data HV
+type PtrHV = Ptr HV
 
 foreign import ccall unsafe
-  init_perl :: IO (Ptr PerlInterpreter)
+  init_perl :: IO PtrPerl
 
 foreign import ccall unsafe
-  exit_perl :: Ptr PerlInterpreter -> IO ()
+  exit_perl :: PtrPerl -> IO ()
+
+
+foreign import ccall unsafe
+  "Perl_newSV" perl_newSV :: PtrPerl -> StrLen -> IO PtrSV
+
+
+foreign import ccall unsafe
+  "S_SvREFCNT_dec" s_SvREFCNT_dec :: PtrPerl -> PtrSV -> IO ()
+
+foreign import ccall unsafe
+  "S_SvREFCNT_dec_NN" s_SvREFCNT_dec_NN :: PtrPerl -> PtrSV -> IO ()
+
+foreign import ccall unsafe
+  "S_SvREFCNT_inc" s_SvREFCNT_inc :: PtrSV -> IO PtrSV
+
+foreign import ccall unsafe
+  "S_SvREFCNT_inc_NN" s_SvREFCNT_inc_NN :: PtrSV -> IO PtrSV
+
+foreign import ccall unsafe
+  "S_SvREFCNT_inc_void" s_SvREFCNT_inc_void :: PtrSV -> IO ()
--- src/Perl/Monad.hs
+++ src/Perl/Monad.hs
@@ -3,40 +3,46 @@ module Perl.Monad
   where
 
 import Foreign
+import Control.Monad
 import Data.Functor.Identity
 import Control.Monad.IO.Class
 
 import Perl.Glue
 
-data SV = SV
-data AV = AV
-data HV = HV
-
 data ScopeFrame = ScopeFrame
-  { poolSV :: [Ptr SV]
-  , poolAV :: [Ptr AV]
-  , poolHV :: [Ptr HV]
+  { poolSV :: [PtrSV]
+  , poolAV :: [PtrAV]
+  , poolHV :: [PtrHV]
   }
 emptyFrame = ScopeFrame [] [] []
 
-newtype PerlT s m a = PerlT { unPerlT :: [ScopeFrame] -> m ([ScopeFrame], a) }
+newtype PerlT s m a = PerlT
+  { unPerlT :: PtrPerl -> [ScopeFrame] -> m ([ScopeFrame], a)
+  }
 type Perl s = PerlT s Identity
 
 runPerlT :: MonadIO m => (forall s. PerlT s m a) -> m a
 runPerlT act = do
   perl <- liftIO init_perl
-  (_, a) <- unPerlT (scope act) []
+  (_, a) <- unPerlT (scope act) perl []
   liftIO (exit_perl perl)
   return a
 
-scope :: Monad m => (forall s. PerlT s m a) -> PerlT s m a
-scope (PerlT act) = PerlT $ \scopeFrames -> do
-  (headFrame : otherFrames, a) <- act (emptyFrame : scopeFrames)
-  -- XXX clean headFrame
+scope :: MonadIO m => (forall s. PerlT s m a) -> PerlT s m a
+scope (PerlT act) = PerlT $ \perl scopeFrames -> do
+  let
+    releasePool :: [Ptr a] -> IO ()
+    releasePool ps = forM_ ps $ \p -> s_SvREFCNT_dec_NN perl (castPtr p)
+  (ScopeFrame poolSV poolAV poolHV : otherFrames, a) <- act perl (emptyFrame : scopeFrames)
+  liftIO $ do
+    releasePool poolSV
+    releasePool poolAV
+    releasePool poolHV
   return (otherFrames, a)
 
 instance Monad m => Monad (PerlT s m) where
-  return a = PerlT $ \scopeFrames -> return (scopeFrames, a)
-  f >>= k = PerlT $ \scopeFrames -> do
-    (scopeFrames', a) <- unPerlT f scopeFrames
-    unPerlT (k a) scopeFrames'
+  return a = PerlT $ \perl scopeFrames -> return (scopeFrames, a)
+  f >>= k = PerlT $ \perl scopeFrames -> do
+    (scopeFrames', a) <- unPerlT f perl scopeFrames
+    unPerlT (k a) perl scopeFrames'
+
