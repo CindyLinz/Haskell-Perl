fea4e1d63d6fcf19feb6ea526d39ad8b37610052
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2014-01-27 02:11:40 +0800

    test extract SV value

--- test/TestMonad.hs
+++ test/TestMonad.hs
@@ -2,7 +2,25 @@ module Main where
 
 import Perl.Monad
 import Foreign.C.String
+import Foreign.ForeignPtr
+import Foreign.Marshal.Alloc
 import Control.Monad.IO.Class
 
 main = runPerlT $ do
-  liftIO (newCString "print 'Hi ', rand 10, $/") >>= eval
+  cmd1 <- liftIO $ newCString "print 'Hi ', rand 10, $/"
+  eval cmd1
+  liftIO $ free cmd1
+
+  cmd2 <- liftIO $ newCString "use Scalar::Util qw(dualvar); dualvar 3.5, '^_^'"
+  res2 <- eval cmd2
+  liftIO $ free cmd2
+
+  iv2 <- svToInt res2
+  nv2 <- svToNum res2
+  (fpStr2, len2) <- svToStr res2
+  liftIO $ do
+    putStrLn $ "iv = " ++ show iv2
+    putStrLn $ "nv = " ++ show nv2
+    withForeignPtr fpStr2 $ \pStr2 -> do
+      str <- peekCStringLen (pStr2, fromIntegral len2)
+      putStrLn $ "pv = " ++ str
