897c8add9adf3d089e85cbaea9beff0edaf48ff8
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2014-02-07 18:41:28 +0800

    refactor class FromSV without explicit m

--- src/Perl/FromSV.hs
+++ src/Perl/FromSV.hs
@@ -10,20 +10,20 @@ import Perl.Type
 import Perl.Monad
 import Perl.MonadGlue
 
-class FromSV m a where
-  fromSV :: PtrSV -> PerlT s m a
+class FromSV a where
+  fromSV :: MonadIO m => PtrSV -> PerlT s m a
 
-instance MonadIO m => FromSV m Int where
+instance FromSV Int where
   fromSV sv = do
     int <- svToInt sv
     return (fromIntegral int)
 
-instance MonadIO m => FromSV m Double where
+instance FromSV Double where
   fromSV sv = do
     CDouble double <- svToNum sv
     return double
 
-instance MonadIO m => FromSV m String where
+instance FromSV String where
   fromSV sv = do
     cStrLen <- svToStr sv
     liftIO $ peekCStringLen cStrLen
