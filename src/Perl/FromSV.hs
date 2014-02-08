d98d10bdb9a17436fd467bb8305196785053c821
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2014-02-08 16:20:58 +0800

    FromSV with fromSVNon

--- src/Perl/FromSV.hs
+++ src/Perl/FromSV.hs
@@ -11,24 +11,29 @@ import Perl.Monad
 import Perl.MonadGlue
 
 class FromSV a where
+  fromSVNon :: MonadIO m => PerlT s m a
   fromSV :: MonadIO m => PtrSV -> PerlT s m a
 
 instance FromSV PtrSV where
+  fromSVNon = newSV 0
   fromSV sv = do
     incRefCnt sv
     return sv
 
 instance FromSV Int where
+  fromSVNon = return 0
   fromSV sv = do
     int <- svToInt sv
     return (fromIntegral int)
 
 instance FromSV Double where
+  fromSVNon = return 0
   fromSV sv = do
     CDouble double <- svToNum sv
     return double
 
 instance FromSV String where
+  fromSVNon = return ""
   fromSV sv = do
     cStrLen <- svToStr sv
     liftIO $ peekCStringLen cStrLen
