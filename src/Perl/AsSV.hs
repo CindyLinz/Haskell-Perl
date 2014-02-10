5756022c5610f2efcfbd7547888abb9a15ff87f5
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2014-02-10 15:30:06 +0800

    AsSV

---
+++ src/Perl/AsSV.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
+module Perl.AsSV
+  where
+
+import Control.Monad.IO.Class
+import Foreign.Ptr
+
+import Perl.Type
+import Perl.Monad
+
+class AsSV a where
+  asSV :: MonadIO m => a -> PerlT s m PtrSV
+
+instance AsSV PtrSV where asSV = return
+instance AsSV RefSV where asSV = return . castPtr
+instance AsSV RefAV where asSV = return . castPtr
+instance AsSV RefHV where asSV = return . castPtr
