b3ab4d57b8bd0f31eb11ace87f717e7ec4b2f8b1
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2014-02-10 18:05:17 +0800

    AsRef

---
+++ src/Perl/AsRef.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
+module Perl.AsRef
+  where
+
+import Control.Monad.IO.Class
+import Foreign.Ptr
+import Foreign.C.Types
+
+import Perl.Type
+import Perl.Constant
+import Perl.Monad
+import Perl.MonadGlue
+
+class AsRef a where
+  safeAsRef :: (MonadIO m, Monad n) => PtrSV -> PerlT s m (n a)
+  asRef :: MonadIO m => PtrSV -> PerlT s m a
+  asRef sv = do
+    maybeRv <- safeAsRef sv
+    case maybeRv of
+      Just rv -> return rv
+      Nothing -> fail "asRv fail"
+
+safeTestRV :: (MonadIO m, Monad n) => (CInt -> Bool) -> PtrSV -> PerlT s m (n (Ptr a))
+safeTestRV pred sv = do
+  t <- rvType sv
+  return $ if pred t
+    then return $ castPtr sv
+    else fail ""
+
+instance AsRef RefSV where
+  safeAsRef = safeTestRV (< const_SVt_PVAV)
+
+instance AsRef RefAV where
+  safeAsRef = safeTestRV (== const_SVt_PVAV)
+
+instance AsRef RefHV where
+  safeAsRef = safeTestRV (== const_SVt_PVHV)
+
+instance AsRef RefCV where
+  safeAsRef = safeTestRV (== const_SVt_PVCV)
+
+--instance AsRef RefGV where
+--  safeAsRef = safeTestRV (== const_SVt_PVGV)
