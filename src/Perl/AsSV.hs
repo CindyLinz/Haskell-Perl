523f252e206edd9a193170dbf6a9808b91bdff29
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2014-02-11 15:10:52 +0800

    AsSVObj

--- src/Perl/AsSV.hs
+++ src/Perl/AsSV.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, ExistentialQuantification #-}
 module Perl.AsSV
   where
 
@@ -11,9 +11,14 @@ import Perl.Monad
 class AsSV a where
   asSV :: MonadIO m => a -> PerlT s m SV
 
+instance AsSV AsSVObj where
+  asSV (AsSVObj a) = asSV a
+
 instance AsSV SV where asSV = return
 instance AsSV CV where asSV = return . castPtr
 instance AsSV RefSV where asSV = return . castPtr
 instance AsSV RefAV where asSV = return . castPtr
 instance AsSV RefHV where asSV = return . castPtr
 instance AsSV RefCV where asSV = return . castPtr
+
+data AsSVObj = forall a. AsSV a => AsSVObj a
