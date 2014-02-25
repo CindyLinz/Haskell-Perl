cf1484531ce5ca7e5fa91258ce8d881f3c93577d
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2014-02-26 02:45:25 +0800

    SVArray

---
+++ src/Perl/SVArray.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE Rank2Types #-}
+module Perl.SVArray
+  ( SVArray
+  , SVArrayBuilder
+  , SVArrayBreaker
+  , addToSV
+  , addFromSV
+  , buildSVArray
+  , breakSVArray
+  ) where
+
+import Control.Monad
+import Control.Monad.IO.Class
+
+import Data.Monoid
+import Data.Array.MArray
+import Data.Array.Storable
+
+import Perl.Type
+import Perl.Monad
+import Perl.SV
+
+type SVArray = StorableArray Int SV
+
+newtype SVArrayBuilder = SVArrayBuilder { unSVArrayBuilder :: [ToSVObj] -> [ToSVObj] }
+newtype SVArrayBreaker s m = SVArrayBreaker { unSVArrayBreaker :: [[SV] -> PerlT s m [SV]] -> [[SV] -> PerlT s m [SV]] }
+
+instance Monoid SVArrayBuilder where
+  mempty = SVArrayBuilder id
+  mappend former later = SVArrayBuilder $ unSVArrayBuilder former . unSVArrayBuilder later
+
+instance Monoid (SVArrayBreaker s m) where
+  mempty = SVArrayBreaker id
+  mappend former later = SVArrayBreaker $ unSVArrayBreaker former . unSVArrayBreaker later
+
+addToSV :: ToSV a => a -> SVArrayBuilder
+addToSV a = SVArrayBuilder (ToSVObj a :)
+
+addFromSV :: ([SV] -> PerlT s m [SV]) -> SVArrayBreaker s m
+addFromSV r = SVArrayBreaker (r :)
+
+buildSVArray :: MonadIO m => SVArrayBuilder -> PerlT s m SVArray
+buildSVArray builder = do
+  svList <- forM (unSVArrayBuilder builder []) toSV
+  liftIO $ newListArray (1, length svList) svList
+
+-- return the unconsumed SVs
+breakSVArray :: MonadIO m => SVArrayBreaker s m -> SVArray -> PerlT s m [SV]
+breakSVArray breakers svArray = do
+  svList <- liftIO $ getElems svArray
+  foldM (\svs br -> br svs) svList (unSVArrayBreaker breakers [])
