4cc36684e55ca8f49144ddf92bfb1960a53f16f2
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2014-03-22 17:01:53 +0800

    deref with type check by exception

--- src/Perl/Ref.hs
+++ src/Perl/Ref.hs
@@ -12,6 +12,17 @@ import Perl.Type
 import Perl.Constant
 import Perl.Monad
 import Perl.Internal.MonadGlue
+import Perl.SV
+
+avoidNull :: (MonadCatch m, MonadIO m) => PerlT s m (Ptr a) -> PerlT s m (Ptr a)
+avoidNull svM = do
+  sv <- svM
+  if sv == nullPtr
+    then do
+      let msg = "deref to NULL"
+      errSV <- toSVMortal msg
+      throwM $ PerlException msg errSV
+    else return sv
 
 class Refable a b | a -> b, b -> a where
   newRef :: (MonadCatch m, MonadIO m) => a -> PerlT s m b
@@ -19,24 +30,29 @@ class Refable a b | a -> b, b -> a where
 
 instance Refable SV RefSV where
   newRef = newSVRef
-  deRef = deRefSV
+  deRef = avoidNull . deRefSV
 
 instance Refable AV RefAV where
   newRef = newAVRef
-  deRef = deRefAV
+  deRef = avoidNull . deRefAV
 
 instance Refable HV RefHV where
   newRef = newHVRef
-  deRef = deRefHV
+  deRef = avoidNull . deRefHV
 
 class AsRef a where
   safeAsRef :: (MonadCatch m, MonadIO m, Monad n) => SV -> PerlT s m (n a)
   asRef :: (MonadCatch m, MonadIO m) => SV -> PerlT s m a
-  asRef sv = do
-    maybeRv <- safeAsRef sv
-    case maybeRv of
-      Just rv -> return rv
-      Nothing -> fail "asRv fail"
+
+asRefCommon :: (MonadCatch m, MonadIO m, AsRef a) => String -> SV -> PerlT s m a
+asRefCommon typeName sv = do
+  maybeRv <- safeAsRef sv
+  case maybeRv of
+    Just rv -> return rv
+    Nothing -> do
+      let msg = "cast " ++ typeName ++ " failed"
+      errSV <- toSVMortal msg
+      throwM $ PerlException msg errSV
 
 safeTestRV :: (MonadCatch m, MonadIO m, Monad n) => (CInt -> Bool) -> SV -> PerlT s m (n (Ptr a))
 safeTestRV pred sv = do
@@ -47,15 +63,19 @@ safeTestRV pred sv = do
 
 instance AsRef RefSV where
   safeAsRef = safeTestRV (< const_SVt_PVAV)
+  asRef = asRefCommon "scalar ref"
 
 instance AsRef RefAV where
   safeAsRef = safeTestRV (== const_SVt_PVAV)
+  asRef = asRefCommon "array ref"
 
 instance AsRef RefHV where
   safeAsRef = safeTestRV (== const_SVt_PVHV)
+  asRef = asRefCommon "hash ref"
 
 instance AsRef RefCV where
   safeAsRef = safeTestRV (== const_SVt_PVCV)
+  asRef = asRefCommon "code ref"
 
 --instance AsRef RefGV where
 --  safeAsRef = safeTestRV (== const_SVt_PVGV)
